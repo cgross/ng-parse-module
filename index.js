ef277c0bb3ab446691f79907c395fb50b8c876e4
Michi Kono <michikono@users.noreply.github.com>
2015-07-16 18:10:18 -0700

    Matching parens should line up with angular.module call

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var save = function(){
     // search for angular.module's indent; if none, look for tabs; if none, use double spaces
     var indent = (indexSearch && indexSearch[1]) || (this.contents.search(/\t/) !== -1) && '\t' || '  ';
     var contents = this.contents.substring(0,
-        this.dependencies.start) + ' [' + '\n' + indent + indent + modules.join(',\n' + indent + indent) + '\n' + indent + ']' +
+        this.dependencies.start) + ' [' + '\n' + indent + indent + modules.join(',\n' + indent + indent) + '\n' + indexSearch + ']' +
         this.contents.substring(this.dependencies.end);
 
     fs.writeFileSync(this.file,contents,'utf8');
